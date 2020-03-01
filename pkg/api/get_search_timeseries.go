0a956dd9ad1a9a6abc248ffcd2f684fa268dfeb6
Masayoshi Mizutani <mizutani@cookpad.com>
2020-03-01 16:46:38 +0900

    Fix timeseries API

--- pkg/api/get_search_timeseries.go
+++ pkg/api/get_search_timeseries.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"time"
+
 	"github.com/aws/aws-sdk-go/service/athena"
 	"github.com/gin-gonic/gin"
 )
@@ -8,7 +10,8 @@ import (
 type getQueryTimeSeriesResponse struct {
 	ID         searchID             `json:"search_id"`
 	MetaData   GetSearchLogMetaData `json:"metadata"`
-	TimeSeries map[string][]int     `json:"timeseries"`
+	TimeSeries map[string][]int64   `json:"timeseries"`
+	Labels     []string             `json:"labels"`
 }
 
 func (x *MinervaHandler) GetSearchTimeSeries(c *gin.Context) (*Response, Error) {
@@ -16,7 +19,7 @@ func (x *MinervaHandler) GetSearchTimeSeries(c *gin.Context) (*Response, Error) 
 
 	id := searchID(c.Param("search_id"))
 	tsData := map[string][]int64{}
-	var tsUnitNum int64 = 20
+	var tsUnitSize int64 = 20
 
 	resp := getQueryTimeSeriesResponse{
 		ID: id,
@@ -29,7 +32,26 @@ func (x *MinervaHandler) GetSearchTimeSeries(c *gin.Context) (*Response, Error) 
 
 	resp.MetaData.searchMetaData = *meta
 
-	var tsMax, tsMin *int64
+	tsMin, tsMax := meta.StartTime, meta.EndTime
+	tsSpan := tsMax - tsMin
+	tsUnitSpan := float64(tsSpan) / float64(tsUnitSize)
+
+	var labelFmt string
+	switch {
+	case tsSpan < 24*3600:
+		labelFmt = "15:04"
+	case tsSpan < 365*24*3600:
+		labelFmt = "Jan-06 15:04"
+	default:
+		labelFmt = "2006-01-02 15:04"
+	}
+
+	for i := int64(0); i < tsUnitSize; i++ {
+		fwd := tsUnitSpan * float64(i)
+		t := time.Unix(tsMin+int64(fwd), 0)
+		resp.Labels = append(resp.Labels, t.Format(labelFmt))
+	}
+
 	if resp.MetaData.Status == athena.QueryExecutionStateSucceeded {
 		ch, err := getLogStream(x.Region, meta.outputPath)
 		if err != nil {
@@ -42,40 +64,22 @@ func (x *MinervaHandler) GetSearchTimeSeries(c *gin.Context) (*Response, Error) 
 				return nil, wrapSystemError(err, 500, "Fail to convert CSV record")
 			}
 
-			if tsMax == nil || *tsMax < log.Timestamp {
-				tsMax = &log.Timestamp
-			}
-			if tsMin == nil || *tsMin > log.Timestamp {
-				tsMin = &log.Timestamp
-			}
-
 			arr, ok := tsData[log.Tag]
 			if !ok {
-				arr = []int64{}
+				arr = make([]int64, tsUnitSize)
 				tsData[log.Tag] = arr
 			}
-			arr = append(arr, log.Timestamp)
-		}
 
-		if tsMax == nil || tsMin == nil {
-			return nil, newUserErrorf(404, "No log data available")
-		}
-
-		tsSpan := *tsMax - *tsMin
-		tsUnitSize := tsSpan / tsUnitNum
-
-		for tag, arr := range tsData {
-			ts := make([]int, tsUnitSize)
-
-			for _, t := range arr {
-				idx := (t - *tsMin) / tsSpan
-				ts[idx]++
+			idx := int(float64(log.Timestamp-tsMin) / tsUnitSpan)
+			if idx >= len(arr) {
+				idx = len(arr) - 1
 			}
-
-			resp.TimeSeries[tag] = ts
+			arr[idx]++
 		}
-
 	}
+
+	resp.TimeSeries = tsData
+
 	Logger.WithField("resp", resp).Debug("Done getSearchLogs")
 
 	return &Response{200, &resp}, nil
