abcd8b707ceed36204a3c2e2dee70e93c6925e96
Masayoshi Mizutani <mizutani@cookpad.com>
2020-05-09 13:06:06 +0900

    Fix CDK errors

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -8,27 +8,11 @@ import * as iam from "@aws-cdk/aws-iam";
 
 const app = new cdk.App();
 
-const bucket = s3.Bucket.fromBucketArn(
-  app,
-  "dataBucket",
-  "arn:aws:s3:::" + process.env.MINERVA_S3_BUCKET
-);
-const topic = sns.Topic.fromTopicArn(
-  app,
-  "dataTopic",
-  process.env.MINERVA_SNS_TOPIC_ARN!
-);
-const lambdaRole = iam.Role.fromRoleArn(
-  app,
-  "LambdaRole",
-  process.env.MINERVA_LAMBDA_ROLE_ARN!,
-  { mutable: false }
-);
-
 new MinervaStack(app, "MyMinervaStack", {
-  dataS3Bucket: bucket,
+  dataS3Region: process.env.MINERVA_S3_REGION!,
+  dataS3Bucket: process.env.MINERVA_S3_BUCKET!,
   dataS3Prefix: process.env.MINERVA_S3_PREFIX!,
   athenaDatabaseName: process.env.MINERVA_ATHENA_DB_NAME!,
-  dataSNSTopic: topic,
-  lambdaRole: lambdaRole,
+  dataSNSTopicARN: process.env.MINERVA_SNS_TOPIC_ARN!,
+  lambdaRoleARN: process.env.MINERVA_LAMBDA_ROLE_ARN!,
 });
--- lib/minerva-stack.ts
+++ lib/minerva-stack.ts
@@ -6,19 +6,18 @@ import * as iam from "@aws-cdk/aws-iam";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as sqs from "@aws-cdk/aws-sqs";
 import * as glue from "@aws-cdk/aws-glue";
-import * as subscriptions from "@aws-cdk/aws-sns-subscriptions";
 import * as apigateway from "@aws-cdk/aws-apigateway";
 
-import {
-  DynamoEventSource,
-  SqsEventSource,
-} from "@aws-cdk/aws-lambda-event-sources";
+import { SqsSubscription } from "@aws-cdk/aws-sns-subscriptions";
+import { SqsEventSource } from "@aws-cdk/aws-lambda-event-sources";
+import { countResourcesLike } from "@aws-cdk/assert";
 
 interface MinervaArguments {
-  lambdaRole: iam.IRole;
-  dataS3Bucket: s3.IBucket;
+  lambdaRoleARN: string;
+  dataS3Region: string;
+  dataS3Bucket: string;
   dataS3Prefix: string;
-  dataSNSTopic: sns.ITopic;
+  dataSNSTopicARN: string;
   athenaDatabaseName: string;
   sentryDSN?: string;
   sentryEnv?: string;
@@ -34,6 +33,25 @@ export class MinervaStack extends cdk.Stack {
     props?: cdk.StackProps
   ) {
     super(scope, id, props);
+
+    const lambdaRole = iam.Role.fromRoleArn(
+      this,
+      "LambdaRole",
+      args.lambdaRoleARN,
+      {
+        mutable: false,
+      }
+    );
+    const dataBucket = s3.Bucket.fromBucketArn(
+      this,
+      "dataBucket",
+      args.dataS3Bucket
+    );
+    const dataTopic = sns.Topic.fromTopicArn(
+      this,
+      "dataTopic",
+      args.dataSNSTopicARN
+    );
 
     const buildPath = lambda.Code.asset("./build");
     const indexTableName = "indices";
@@ -59,30 +77,29 @@ export class MinervaStack extends cdk.Stack {
     const indexerQueue = new sqs.Queue(this, "indexerQueue", {
       visibilityTimeout: cdk.Duration.seconds(600),
     });
-    args.dataSNSTopic.addSubscription(
-      new subscriptions.SqsSubscription(indexerQueue)
-    );
+    dataTopic.addSubscription(new SqsSubscription(indexerQueue));
+    /*
+    const indexerQueuePolicy = new sqs.QueuePolicy(this, "indexerQueuePolicy", {
+      queues: [indexerQueue],
+    });
+    */
 
     const mergeQueue = new sqs.Queue(this, "mergeQueue", {
       visibilityTimeout: cdk.Duration.seconds(450),
     });
     const partitionQueue = new sqs.Queue(this, "partitionQueue");
 
-    const indexerQueuePolicy = new sqs.QueuePolicy(this, "indexerQueuePolicy", {
-      queues: [indexerQueue],
-    });
-
     // Lambda Functions
     const indexer = new lambda.Function(this, "indexer", {
       runtime: lambda.Runtime.GO_1_X,
       handler: "indexer",
       code: buildPath,
-      role: args.lambdaRole,
+      role: lambdaRole,
       timeout: cdk.Duration.seconds(600),
       memorySize: 2048,
       environment: {
-        S3_REGION: args.dataS3Bucket.bucketRegionalDomainName,
-        S3_BUCKET: args.dataS3Bucket.bucketName,
+        S3_REGION: args.dataS3Region,
+        S3_BUCKET: args.dataS3Bucket,
         S3_PREFIX: args.dataS3Prefix,
         INDEX_TABLE_NAME: indexTableName,
         MESSAGE_TABLE_NAME: messageTableName,
@@ -97,12 +114,12 @@ export class MinervaStack extends cdk.Stack {
       runtime: lambda.Runtime.GO_1_X,
       handler: "listIndexObject",
       code: buildPath,
-      role: args.lambdaRole,
+      role: lambdaRole,
       timeout: cdk.Duration.seconds(300),
       memorySize: 1024,
       environment: {
-        S3_REGION: args.dataS3Bucket.bucketRegionalDomainName,
-        S3_BUCKET: args.dataS3Bucket.bucketName,
+        S3_REGION: args.dataS3Region,
+        S3_BUCKET: args.dataS3Bucket,
         S3_PREFIX: args.dataS3Prefix,
         MERGE_QUEUE: mergeQueue.queueUrl,
       },
@@ -114,7 +131,7 @@ export class MinervaStack extends cdk.Stack {
       runtime: lambda.Runtime.GO_1_X,
       handler: "mergeIndexObject",
       code: buildPath,
-      role: args.lambdaRole,
+      role: lambdaRole,
       timeout: cdk.Duration.seconds(450),
       memorySize: 2048,
       reservedConcurrentExecutions: args.concurrentExecution,
@@ -125,14 +142,14 @@ export class MinervaStack extends cdk.Stack {
       runtime: lambda.Runtime.GO_1_X,
       handler: "makePartition",
       code: buildPath,
-      role: args.lambdaRole,
-      timeout: cdk.Duration.seconds(450),
+      role: lambdaRole,
+      timeout: cdk.Duration.seconds(30),
       memorySize: 2048,
       environment: {
         ATHENA_DB_NAME: args.athenaDatabaseName,
         OBJECT_TABLE_NAME: indexTableName,
         META_TABLE_NAME: metaTable.tableName,
-        S3_BUCKET: args.dataS3Bucket.bucketName,
+        S3_BUCKET: args.dataS3Bucket,
         S3_PREFIX: args.dataS3Prefix,
       },
       reservedConcurrentExecutions: args.concurrentExecution,
@@ -155,7 +172,7 @@ export class MinervaStack extends cdk.Stack {
         { name: "object_id", type: glue.Schema.BIG_INT },
         { name: "seq", type: glue.Schema.INTEGER },
       ],
-      bucket: args.dataS3Bucket,
+      bucket: dataBucket,
       s3Prefix: args.dataS3Prefix + "indices/",
       dataFormat: glue.DataFormat.PARQUET,
     });
@@ -170,7 +187,7 @@ export class MinervaStack extends cdk.Stack {
         { name: "seq", type: glue.Schema.INTEGER },
         { name: "message", type: glue.Schema.STRING },
       ],
-      bucket: args.dataS3Bucket,
+      bucket: dataBucket,
       s3Prefix: args.dataS3Prefix + "messages/",
       dataFormat: glue.DataFormat.PARQUET,
     });
@@ -180,12 +197,12 @@ export class MinervaStack extends cdk.Stack {
       runtime: lambda.Runtime.GO_1_X,
       handler: "apiHandler",
       code: buildPath,
-      role: args.lambdaRole,
+      role: lambdaRole,
       timeout: cdk.Duration.seconds(120),
       memorySize: 2048,
       environment: {
-        S3_REGION: args.dataS3Bucket.bucketRegionalDomainName,
-        S3_BUCKET: args.dataS3Bucket.bucketName,
+        S3_REGION: args.dataS3Region,
+        S3_BUCKET: args.dataS3Bucket,
         S3_PREFIX: args.dataS3Prefix,
         ATHENA_DB_NAME: indexDB.databaseName,
         INDEX_TABLE_NAME: indexTableName,
@@ -213,12 +230,12 @@ export class MinervaStack extends cdk.Stack {
 
     const v1 = api.root.addResource("api").addResource("v1");
     const searchAPI = v1.addResource("search");
+
     searchAPI.addMethod("POST");
-    searchAPI.addResource("{search_id}").addMethod("GET");
-    searchAPI.addResource("{search_id}").addResource("logs").addMethod("GET");
-    searchAPI
-      .addResource("{search_id}")
-      .addResource("timeseries")
-      .addMethod("GET");
+
+    const searchAPIwithID = searchAPI.addResource("{search_id}");
+    searchAPIwithID.addMethod("GET");
+    searchAPIwithID.addResource("logs").addMethod("GET");
+    searchAPIwithID.addResource("timeseries").addMethod("GET");
   }
 }
